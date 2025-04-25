a610270d4dd4588fc3f76815779e9d64f6e08f9d
Cyprien <cyprien.duroy@etu.unicaen.fr>
2025-04-25 16:49:50 +0200

    Creating the basic TeamView page

---
+++ src/views/TeamPage.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue';
+
+</script>
+<template>
+  <div class="team-view">
+    <Navbar/>
+
+    <div class="team-container">
+      <h1>Mon équipe</h1>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.team-container {
+  padding: 20px;
+  max-width: 800px;
+  margin: 0 auto;
+  background: #f9f9f9;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+</style>
